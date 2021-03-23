2b7d856810fcfb98fd3d83366526dd0e81e62d1a
Ethan <ethancaff@gmail.com>
2021-03-23 15:35:02 +0000

    Added Administrator Class
    Added administrator class that inherits from the abstract person class.

---
+++ Student_Assignment_System/Administrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Assignment_System
+{
+    class Administrator : Person
+    {
+        public string AdminID { get; set; }
+        public string AdminPassword { get; set; }
+        public int PhoneNumber { get; set; }
+        public string DateOfHire { get; set; }
+
+        public Administrator(string ID, string password, int pNumber, string HireDate, string name, string dob, string address, string ppsn) : base(name,dob,address,ppsn)
+        {
+            this.AdminID = ID;
+            this.AdminPassword = password;
+            this.PhoneNumber = pNumber;
+            this.DateOfHire = HireDate;
+        }
+    }
+}
