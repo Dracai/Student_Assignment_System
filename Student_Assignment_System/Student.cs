b38b40a66b6111d5b27e4ae8a1d36e53996545c1
Kuba <61418085+Dracai@users.noreply.github.com>
2021-03-23 15:35:11 +0000

    Student Class
    Please work, needs work

---
+++ Student_Assignment_System/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Assignment_System
+{
+    class Student : Person
+    {
+        public string StudentID { get; set; }
+        public string StudentPassword { get; set; }
+        public string ClassGroup { get; set; }
+        public string DateEnrolled { get; set; }
+
+        public Student(string pName, string pDOB, string pAddress, string pPPSNumber, 
+            string sSID, string sPW, string sClassGroup, string sDateEnrolled) : base(pName, pDOB, pAddress, pPPSNumber)
+        {
+            this.StudentID = sSID;
+            this.StudentPassword = sPW;
+            this.ClassGroup = sClassGroup;
+            this.DateEnrolled = sDateEnrolled;
+        }
+    }
+}
